813d9ffba46006060faf1c4c72fe05eba29f3f00
Thomas Rasch <thomas.rasch@alpstein.com>
2015-02-06 12:59:39 +0100

    o Bugfixes for the MongoDB driver

--- mbutil/database.py
+++ mbutil/database.py
@@ -1565,7 +1565,7 @@ class MBTilesMongoDB(MBTilesDatabase):
                             sys.exit(1)
 
             self.connect_options = dict(option.split("=") for option in connect_string.strip().split(" "))
-            self.connect_string = "mongodb://%s:%s@%s/admin" % (self.connect_options['user'], self.connect_options['password'], self.connect_options['hostaddr'])
+            self.connect_string = "mongodb://%s:%s@%s/admin" % (self.connect_options['user'], self.connect_options['password'], self.connect_options.get('hostaddr', self.connect_options['host']))
 
             self.con = pymongo.mongo_client.MongoClient(self.connect_string)
             self.cur = self.con[self.connect_options['dbname']]
@@ -1622,7 +1622,7 @@ class MBTilesMongoDB(MBTilesDatabase):
             query["t"] = t_query
 
         if len(query):
-            return self.con.tiles.find(query).count()
+            return self.cur.tiles.find(query).count()
 
         return self.cur.tiles.count()
 
